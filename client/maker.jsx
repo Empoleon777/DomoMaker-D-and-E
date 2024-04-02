7bd83b526c1f3aa9d89cbfd437364c60889b4b03
William Fox <bcf7023@rit.edu>
2024-04-02 11:02:12 -0400

    Great. What now?

--- client/maker.jsx
+++ client/maker.jsx
@@ -37,7 +37,7 @@ const DomoForm = (props) => {
 }
 
 const DomoList = (props) => {
-    if (props.domo.length === 0) {
+    if (props.domos.length === 0) {
         return (
             <div className="domoList">
                 <h3 className="emptyDomo">No Domos Yet!</h3>
@@ -47,7 +47,7 @@ const DomoList = (props) => {
 
     const domoNodes = props.domos.map(domo => {
         return (
-            <div key={domo._id} classname="domo">
+            <div key={domo._id} className="domo">
                 <img src="/assets/img/domoface.jpeg" alt="domo face" className="domoFace" />
                 <h3 className="domoName"> Name: {domo.name} </h3>
                 <h3 className="domoAge"> Age: {domo.age} </h3>
